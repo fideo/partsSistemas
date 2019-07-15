e61852de83867272fc44b16498d683a3c37426ab
Federico Mazzei <federicomazzei@gmail.com>
2019-07-15 16:54:35 -0300

    agrego texto a transporte_publico

--- pages/transporte_publico.vue
+++ pages/transporte_publico.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
-        <h1>Transporte Público</h1>
-        <v-layout>
+  <v-layout>
     <v-flex xs12 sm10 offset-sm1>
       <v-card>
         <v-img
@@ -12,22 +11,18 @@
           <v-container fill-height fluid>
             <v-layout fill-height>
               <v-flex xs12 align-end flexbox>
-                <span class="headline">Top 10 Australian beaches</span>
+                <h1 class="blue-grey--text text--darken-4 ">Transporte Público</h1>
               </v-flex>
             </v-layout>
           </v-container>
         </v-img>
         <v-card-title>
-          <div>
-            <span class="grey--text">Number 10</span><br>
-            <span>Whitehaven Beach</span><br>
-            <span>Whitsunday Island, Whitsunday Islands</span>
+          <div class="pt-3">
+            <h2 class="pt-3">Parts Sistemas presenta su nueva generación de soluciones inteligentes para el transporte público de pasajeros donde el límite lo define usted</h2>
+            <p class="pt-4">Comenzamos nuestro camino siendo los responsables de informatizar, por primera vez, la venta de pasajes de los ferrocarriles más importantes de nuestro país. Aceptamos el desafio y diseñamos, fabricamos e instalamos más de 300 expendedoras de tickets que funcionan como una gran red interconectada de venta y recarga de tarjetas de pasajes</p>
+            <p class="pt-2">Mucho tiempo ha pasado desde nuestras primeras expendedoras de pasajes y grandes cambios se han producido en las áreas de tecnología e ingeniería. A la fecha, nuestros ingenieros han desarrollado más de 15 modelos distintos de expendedoras de pasajes, molinetes de alto tránsito, carteles de información y sistemas de control de flota y seguimiento satelital</p>
           </div>
         </v-card-title>
-        <v-card-actions>
-          <v-btn flat color="orange">Share</v-btn>
-          <v-btn flat color="orange">Explore</v-btn>
-        </v-card-actions>
       </v-card>
     </v-flex>
   </v-layout>
@@ -40,6 +35,9 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+p::first-letter {
+  font-weight: bold;
+  color: #FFAB00;
+}
 </style>
